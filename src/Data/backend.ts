c82d70e5c206df64a2a0322f051c92360186003f
Kyrua <ang02aezs@gmail.com>
2024-09-13 14:17:18 -0600

    python-django technologies added

--- src/Data/backend.ts
+++ src/Data/backend.ts
@@ -5,5 +5,7 @@ export const backend = [
      { name: "NestJS", class: "devicon-nestjs-plain" },
      { name: ".NET Core", class: "devicon-dotnetcore-plain" },
      { name: "ASP.NET"},
-     { name: "Spring", class: "devicon-spring-original"}
+     { name: "Spring", class: "devicon-spring-original"},
+     { name: "Django", class: "devicon-django-plain"},
+     { name: "DjangoRest", class:"devicon-djangorest-line-wordmark"},
 ]
--- src/Data/languages.ts
+++ src/Data/languages.ts
@@ -3,5 +3,6 @@ export const languages = [
      { name: "TypeScript", class: "devicon-typescript-plain" },
      { name: "JavaScript", class: "devicon-javascript-plain" },
      { name: "C#", class: "devicon-csharp-plain" },
-     { name: "Java", class: "devicon-java-plain"}
+     { name: "Java", class: "devicon-java-plain"},
+     { name: "Python", class: "devicon-python-plain"}
 ]
