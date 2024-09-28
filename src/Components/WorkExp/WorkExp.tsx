7b780e84fd0e90bca326afd415763af752887eb6
Kyrua <ang02aezs@gmail.com>
2024-09-28 01:49:52 -0600

    work-exp updated

--- src/Components/WorkExp/WorkExp.tsx
+++ src/Components/WorkExp/WorkExp.tsx
@@ -7,9 +7,19 @@ const WorkExp = () => {
                <div className='divInfoExperience'>
                     <img src="https://the-fourth-element-client.vercel.app/_next/static/media/logo.dc0a4bbc.svg" alt="" />
                     <div className='divInfoExperienceText'> 
-                    <h3> 10 / 2023 - 12 / 2023 </h3>
+                    <h3> 09 / 2023 - 11 / 2023 </h3>
                     <p>
-                    I worked for 2 months as a frontend developer in a web application of courses for high performance athletes, owned by Estanislao Bachrach. I gained experience in design and development of attractive interfaces using technologies such as JavaScript, TypeScript, React, NEXT.js, Zustand, Sass, Tailwind and Figma.
+                    I worked for 2 months as a frontend developer in a web application of courses for high performance athletes, owned by Estanislao Bachrach.
+                    </p>
+                    </div>
+               </div>
+               <div className='divInfoExperience'>
+                    <h3>Freelance</h3>
+                    <div className='divInfoExperienceText'> 
+                    <h3> 12 / 2023 - Present </h3>
+                    <p>
+                    I develop complete web solutions as a freelance
+                    developer, covering both front-end and back-end.
                     </p>
                     </div>
                </div>
